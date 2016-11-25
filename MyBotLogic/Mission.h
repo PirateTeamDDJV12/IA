17381ad7a638529dd64bc3830d4923235d1260ed
Constant Brunel <constant.95@gmail.com>
2016-11-25 12:06:51 -0500

    Begin Missions manager

---
+++ MyBotLogic/Mission.h
@@ -0,0 +1,53 @@
+#ifndef _MISSION_H_
+#define _MISSION_H_
+
+class Mission
+{
+    enum MissionType
+    {
+        CHANGE_ZONE,
+        OPEN_DOOR,
+    };
+private:
+    MissionType m_mType;
+    unsigned m_objective;
+    Mission* m_prev = nullptr;
+    Mission* m_next = nullptr;
+    bool m_status = false;
+public:
+
+    Mission(MissionType t, unsigned obj)
+        : m_mType{t}, m_objective{obj}
+    {}
+
+    unsigned getObjective() const
+    {
+        return m_objective;
+    }
+
+    void setDependanceMission(Mission *m)
+    {
+        m_next = m;
+        m_prev = this;
+    }
+
+    bool isSuccess() const
+    {
+        return m_status;
+    }
+
+    void success()
+    {
+        m_status = true;
+        delete m_next;
+        m_next = nullptr;
+    }
+
+private:
+    void setSuccesDependanceMission() const
+    {
+        m_next->success();
+    }
+};
+
+#endif //!_MISSION_H_
