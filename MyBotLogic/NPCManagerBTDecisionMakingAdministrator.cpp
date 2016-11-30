3fc202c2a558c60a3d12ac084d2c14c5656873f0
SunlayGGX <sunlay@evotion.fr>
2016-11-30 16:54:00 -0500

    First Decision Making BT Layer done
    Not tested.
    
    First Layer done => has a goal ? yes : A*, no : to second Layer

--- MyBotLogic/NPCManagerBTDecisionMakingAdministrator.cpp
+++ MyBotLogic/NPCManagerBTDecisionMakingAdministrator.cpp
@@ -8,5 +8,42 @@ using namespace BehaviourTree;
 
 void NPCManagerBTDecisionMakingAdministrator::init()
 {
-    // TODO
+    BlocTernarySelector* ternarySelectorRoot = m_behaviorTreeRoot.as<BlocTernarySelector>();
+
+
+    // First Layer => has a goal? 
+    // yes : assign an AStar
+    // no : go to next layer : is the area explored ?
+
+    // Has a goal check bloc
+    ternarySelectorRoot->connect(
+        BlocFabric::createGeneralAction(
+            []() {
+                // TODO
+                return /*Has a goal*/true ? general::result::SUCCESS : general::result::FAIL;
+            },
+            "HasAGoal"
+        )
+    );
+
+    // YES : assign AStar
+    ternarySelectorRoot->connect(
+        BlocFabric::createGeneralAction(
+            []() {
+                // TODO
+            },
+            "AStar"
+        )
+    );
+
+
+    // Second Layer node creation
+    BlocRef exploredAreaTernaryRef = BlocFabric::createCompositeBloc<BlocTernarySelector>("ExploredAreaNode");
+
+    BlocTernarySelector* exploredAreaTernarySelector = exploredAreaTernaryRef->as<BlocTernarySelector>();
+
+
+
+    // connection of the first layer with the second layer (NO case)
+    ternarySelectorRoot->connect(exploredAreaTernaryRef);
 }
