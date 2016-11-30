2ee6039e4c6c7c5ebfdd8274ae27621c2934a2d5
Alexandre Sendra <sendra.alexandre@gmail.com>
2016-11-29 22:23:07 -0500

    remove useless debug

--- MyBotLogic/MyBotLogic.cpp
+++ MyBotLogic/MyBotLogic.cpp
@@ -68,26 +68,11 @@ MyBotLogic::MyBotLogic()
     BOT_LOGIC_LOG(mLogger, "\nTURN #" + std::to_string(++m_turnCount), true);
 
     Map *myMap = Map::get();
-    std::stringstream ss;
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
+   
     // Update graph
     myMap->updateEdges(_turnInfo.objects, m_turnCount);
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    ss << " UpdateEdges : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
-
-    begin = std::chrono::steady_clock::now();
     myMap->updateTiles(_turnInfo.tiles);
-    end = std::chrono::steady_clock::now();
-    ss << " UpdateTiles : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
-
-    begin = std::chrono::steady_clock::now();
     myMap->updateZones(_turnInfo.npcs, NPCManager::get()->getNpcs());
-    end = std::chrono::steady_clock::now();
-    ss << " UpdateZones : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " FINI" << std::endl;
-    OutputDebugString(ss.str().c_str());
-    BOT_LOGIC_MAP_LOG(m_loggerInfluence, myLog, false);
-    ss.clear();
 
     // Create Influence map
     myMap->createInfluenceMap();
@@ -99,6 +84,7 @@ MyBotLogic::MyBotLogic()
 
     // Update all NPCs and fill the action list
     NPCManager::get()->updateNPCs(_actionList);
+
 }
 
 /*virtual*/ void MyBotLogic::Exit()
