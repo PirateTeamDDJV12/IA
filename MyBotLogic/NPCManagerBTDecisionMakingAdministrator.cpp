925e62dd6c68ef11d2cf0b5114fb19c73a96c0f3
SunlayGGX <sunlay@evotion.fr>
2016-11-30 17:01:54 -0500

    Second decision making BT layer done
    Not tested yet
    
    I've done the second layer :
    The area is explored ? yes : go to the third layer, no : ordinary explore

--- MyBotLogic/NPCManagerBTDecisionMakingAdministrator.cpp
+++ MyBotLogic/NPCManagerBTDecisionMakingAdministrator.cpp
@@ -46,4 +46,46 @@ void NPCManagerBTDecisionMakingAdministrator::init()
 
     // connection of the first layer with the second layer (NO case)
     ternarySelectorRoot->connect(exploredAreaTernaryRef);
+
+
+
+    // Second Layer => is the area explored ? 
+    // yes : go to the next layer : a door is there ?
+    // no : regular explore assignement
+
+    // a door is there checker bloc
+    exploredAreaTernarySelector->connect(
+        BlocFabric::createGeneralAction(
+            []() {
+                // TODO
+                return /*is the area explored ?*/true ? general::result::SUCCESS : general::result::FAIL;
+            },
+            "IsTheAreaExplored"
+        )
+    );
+
+
+    // NO : Regular Explore Assignement
+    std::string exploredAreaNoCaseOrRegularExploreBlocName{ "RegularExplore" };
+
+    exploredAreaTernarySelector->connect(
+        BlocFabric::createGeneralAction(
+            []() {
+                // TODO
+            },
+            exploredAreaNoCaseOrRegularExploreBlocName
+        )
+    );
+
+
+    // Third Layer node creation
+    BlocRef aDoorIsThereTernaryRef = BlocFabric::createCompositeBloc<BlocTernarySelector>("aDoorIsThereNode");
+
+    BlocTernarySelector* aDoorIsThereTernarySelector = aDoorIsThereTernaryRef->as<BlocTernarySelector>();
+
+
+    // connection of the second layer with the third layer (YES case)
+
+    // before because the yes case must be before the no case.
+    exploredAreaTernarySelector->connectBefore(aDoorIsThereTernaryRef, exploredAreaNoCaseOrRegularExploreBlocName);
 }
