85d7b40e6a36d5686dfe7a883dd5732eec9401b4
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2016-12-01 16:25:57 -0500

    set id in subscribeGoal and fix error in subscribeMission

--- MyBotLogic/MissionManager.cpp
+++ MyBotLogic/MissionManager.cpp
@@ -8,19 +8,22 @@ void MissionManager::subscribeGoal(unsigned fromZone, unsigned destinationZone)
     m_objectives.push_back(std::make_pair(fromZone, destinationZone));
     //Consulter Planificateur pour obtenir les sous missions
     retourPlanificateur stepsToReachGoal = Planificateur::createMissionsToReachGoal(fromZone, destinationZone);
+    for (Mission submis : stepsToReachGoal)
+    {
+        submis.setId(m_missionsCount);
+        ++m_missionsCount;
+    }
     m_missions.insert(m_missions.end(), stepsToReachGoal.begin(), stepsToReachGoal.end());
 }
 
 void MissionManager::subscribeMission(Mission mission, unsigned int npcId)
 {
     
-    mission.setId(m_missionsCount);
-    ++m_missionsCount;
-    //Consulter Planificateur pour obtenir les sous missions
-    retourPlanificateur subMissions = Planificateur::planifiedMissions(mission, npcId);
+    //Consulter Planificateur pour obtenir les sous missions et les enregistrer
+    retourPlanificateur subMissions = Planificateur::planifyMissions(mission, npcId);
     for (Mission submis : subMissions)
     {
-        mission.setId(m_missionsCount);
+        submis.setId(m_missionsCount);
         ++m_missionsCount;
     }
     m_missions.insert(m_missions.end(), subMissions.begin(), subMissions.end());
