d6dceeb9939cfacb77eb35459e91080f6635feb4
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2016-11-30 21:34:59 -0500

    clean du code et reserve taille max lors du constructeur

--- MyBotLogic/MissionManager.cpp
+++ MyBotLogic/MissionManager.cpp
@@ -2,7 +2,20 @@
 
 MissionManager MissionManager::m_instance;
 
-retourPlanificateur Planificateur::createMissionsToReachGoal(unsigned from, unsigned destination)
+
+void MissionManager::subscribeGoal(unsigned fromZone, unsigned destinationZone)
 {
-    throw std::logic_error("The method or operation is not implemented.");
+    m_objectifs.push_back(std::make_pair(fromZone, destinationZone));
+    //Consulter Planificateur pour obtenir les sous missions
+    retourPlanificateur stepsToReachGoal = Planificateur::createMissionsToReachGoal(fromZone, destinationZone);
+    m_missions.insert(m_missions.end(), stepsToReachGoal.begin(), stepsToReachGoal.end());
+}
+
+void MissionManager::subscribeMission(Mission mission, unsigned int npcId)
+{
+    //Consulter Planificateur pour obtenir les sous missions
+    retourPlanificateur subMissions = Planificateur::planifiedMissions(mission, npcId);
+
+    //enregistrer missions rendues par le planificateur
+    m_missions.insert(m_missions.end(), subMissions.begin(), subMissions.end());
 }
