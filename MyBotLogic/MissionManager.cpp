3abd93eec7f60f31d6cdde5b0c9043fe604c3ef7
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2016-12-01 22:44:51 -0500

    Pb avec les maps, j'ai mis le vect de mission directement dans la struct Obj

--- MyBotLogic/MissionManager.cpp
+++ MyBotLogic/MissionManager.cpp
@@ -2,26 +2,24 @@
 
 MissionManager MissionManager::m_instance;
 
-
+//ajout objectif et découpage en mission
 void MissionManager::subscribeGoal(unsigned fromZone, unsigned destinationZone)
 {
     
-    //m_objectives.push_back(std::make_pair(fromZone, destinationZone));
     //Consulter Planificateur pour obtenir les sous missions
     Objectif newObj = Objectif(m_objectivesCount, fromZone, destinationZone);
+    ++m_objectivesCount;
     retourPlanificateur stepsToReachGoal = Planificateur::createMissionsToReachGoal(newObj);
-    for (Mission submis : stepsToReachGoal)
-    {
-        submis.setId(m_missionsCount);
-        ++m_missionsCount;
-    }
-    m_missions.insert(m_missions.end(), stepsToReachGoal.begin(), stepsToReachGoal.end());
-    m_objectives.insert(std::make_pair(newObj, stepsToReachGoal));
+    //enregistrer missions dans Objectif et register Objectif
+    newObj.m_missionsObjectif.insert(m_missions.end(), stepsToReachGoal.begin(), stepsToReachGoal.end());
+    m_objectives.push_back(newObj);
 }
 
+
+//Ajouter mission sans objectifs
 void MissionManager::subscribeMission(Mission mission, unsigned int npcId)
 {
-    
+
     //Consulter Planificateur pour obtenir les sous missions et les enregistrer
     retourPlanificateur subMissions = Planificateur::planifyMissions(mission, npcId);
     for (Mission submis : subMissions)
--- MyBotLogic/MissionManager.h
+++ MyBotLogic/MissionManager.h
@@ -13,11 +13,23 @@
 //le vrai planificateur sera fait
 typedef std::vector<Mission> retourPlanificateur;
 
+struct Objectif
+{
+    unsigned int m_id;
+    unsigned int m_from;
+    unsigned int m_destinaton;
+    Objectif(unsigned int id, unsigned int from, unsigned int dest) : m_id{ id }, m_from{ from }, m_destinaton{ dest }
+    {}
+    std::vector<Mission> m_missionsObjectif;
+};
+
 
 // A enlever quand le vrai Planif sera fait
 class Planificateur
 {
 public:
+
+
 
 
     static retourPlanificateur planifyMissions(Mission mission, unsigned int npcID)
@@ -35,49 +47,73 @@ public:
 };
 
 
-struct Objectif
-{
-    unsigned int m_id;
-    unsigned int m_from;
-    unsigned int m_destinaton;
-    Objectif(unsigned int id, unsigned int from, unsigned int dest) : m_id{ id }, m_from{ from }, m_destinaton{dest}
-    {}
-};
+
+
 
 //MissionManager
 class MissionManager : public Singleton
 {
 private:
-    
+
     static MissionManager m_instance;
+    //Missions sans objectifs (il yen aura ?)
     retourPlanificateur m_missions;
-    std::map<Objectif, std::vector<Mission>> m_objectives;
+    //objectifs et missions associées
+    std::vector<Objectif> m_objectives;
     enum { MissionMax = 200 };
+
     unsigned int m_missionsCount;
     unsigned int m_objectivesCount;
-    MissionManager() :m_missionsCount{ 0 }, m_objectivesCount{0}
+    MissionManager() :m_missionsCount{ 0 }, m_objectivesCount{ 0 }
     {
         m_missions.reserve(MissionMax);
+        m_objectives.reserve(MissionMax);
     }
 public:
     static MissionManager* get()
     {
         return &m_instance;
     }
-    retourPlanificateur getMissions()
+
+    std::vector<Objectif>* getObjectives()
     {
-        return m_missions;
+        return &m_objectives;
+    }
+
+
+    retourPlanificateur* getMissions()
+    {
+        return &m_missions;
     }
 
     //ajouter un Objectif (Changer de Zone)
     void subscribeGoal(unsigned fromZone, unsigned destinationZone);
-    
-    
+
+
     //Ajouter une mission (Se déplacer/ intéragir)
     void subscribeMission(Mission mission, unsigned int npcId);
-    
-    //Supprimer une mission
-    void deleteMission(unsigned int missionId)
+
+    //Supprimer un Objectif
+    void deleteObjectif(unsigned int idToDelete)
+    {
+        m_objectives.erase(std::remove_if(m_objectives.begin(), m_objectives.end(), [idToDelete](Objectif obj) {return obj.m_id == idToDelete; }));
+    }
+
+    //Supprimer une mission d'un objectif
+
+    void deleteMissionFromObjective(unsigned int idObjective)
+    {
+        auto itObjectiveToCheck = std::find_if(m_objectives.begin(), m_objectives.end(), [idObjective](Objectif obj)
+        {return obj.m_id == idObjective; });
+        //on met la mission à la fin du vector
+        std::remove_if(itObjectiveToCheck->m_missionsObjectif.begin(), itObjectiveToCheck->m_missionsObjectif.end(), [itObjectiveToCheck](Mission mission)
+        {return (mission.getId() == itObjectiveToCheck->m_missionsObjectif.front().getId()); });
+        //et on l'efface
+        itObjectiveToCheck->m_missionsObjectif.erase(itObjectiveToCheck->m_missionsObjectif.end());
+    }
+
+    //Supprimer une mission indépendante
+        void deleteMission(unsigned int missionId)
     {
         //on met la mission à la fin du vector
         std::remove_if(m_missions.begin(), m_missions.end(), [missionId](Mission mission) {return mission.getId() == missionId; });
