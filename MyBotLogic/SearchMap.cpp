402d83d0ce224dbf1d8e4af186a3b42299c4fe10
Constant Brunel <constant.95@gmail.com>
2016-12-06 22:14:19 -0500

    Clean and comment searchMap

--- MyBotLogic/SearchMap.cpp
+++ MyBotLogic/SearchMap.cpp
@@ -13,7 +13,7 @@ void SearchMap::prepareNode(Node* refNode, unsigned int newGValue, SearchNode* p
     unsigned int id = refNode->getId();
     SearchNode* node = new SearchNode(refNode->getPosition()->x, refNode->getPosition()->y, id, parent);
     node->setG(newGValue);
-    node->setH(calculateManathan(node, m_goal));
+    node->setH(calculateManhattan(node, m_goal));
 
     for(int i = 0; i < closedList.size(); i++)
     {
@@ -114,10 +114,10 @@ void SearchMap::initSearchMap(Node* start, Node* goal, std::set<Node::NodeType> 
 
         m_isGoalFound = false;
 
-        setStartNode(new SearchNode(start->getPosition()->x, start->getPosition()->y, start->getId()));
-        setGoalNode(new SearchNode(goal->getPosition()->x, goal->getPosition()->y, goal->getId()));
+        m_start = new SearchNode(start->getPosition()->x, start->getPosition()->y, start->getId());
+        m_goal = new SearchNode(goal->getPosition()->x, goal->getPosition()->y, goal->getId());
 
-        m_start->setH(calculateManathan(m_start, m_goal));
+        m_start->setH(calculateManhattan(m_start, m_goal));
 
         openList.push_back(m_start);
         m_isInitialized = true;
@@ -149,7 +149,7 @@ SearchNode* SearchMap::getNextNodeToSearch()
     return nextNode;
 }
 
-unsigned int SearchMap::calculateManathan(const SearchNode* start, const SearchNode* goal) const
+unsigned int SearchMap::calculateManhattan(const SearchNode* start, const SearchNode* goal) const
 {
     int x = goal->getX() - start->getX();
     int y = goal->getY() - start->getY();
--- MyBotLogic/SearchMap.h
+++ MyBotLogic/SearchMap.h
@@ -9,12 +9,22 @@ class SearchNode;
 
 class SearchMap
 {
+    // The start and goal node for the A*
     SearchNode* m_start;
     SearchNode* m_goal;
+
+    // List of all the open node to search
     std::vector<SearchNode*> openList;
+
+    // List of all the node the algorithm already search
     std::vector<SearchNode*> closedList;
+
+    // All the node to go through the start to the goal
     std::vector<unsigned int> m_pathToGoal;
+
+    // List of the type we can access
     std::set<Node::NodeType> m_forbiddenType;
+
     bool m_isGoalFound = false;
     bool m_isInitialized = false;
 public:
@@ -24,19 +34,20 @@ public:
     SearchMap(Node* start, Node* goal);
     SearchMap(Node* start, Node* goal, std::set<Node::NodeType> forbiddenType);
 
+    // Initialise the map between two nodes
     void initSearchMap(Node*, Node*, std::set<Node::NodeType> forbiddenType = {Node::NodeType::FORBIDDEN, Node::NodeType::OCCUPIED});
+
+    // Execute the search function to get a path
     std::vector<unsigned int> search();
+
+    // Prepare the node to add it in the open list
     void prepareNode(Node*, unsigned int, SearchNode*);
+
+    // Return the next node to be in the closed list (lowest cost)
     SearchNode* getNextNodeToSearch();
-    unsigned int calculateManathan(const SearchNode* start, const SearchNode* goal) const;
-    void setStartNode(SearchNode* n)
-    {
-        m_start = n;
-    }
-    void setGoalNode(SearchNode* n)
-    {
-        m_goal = n;
-    }
+
+    // Calcule the manhattan distance
+    unsigned int calculateManhattan(const SearchNode* start, const SearchNode* goal) const;
 };
 
 #endif // SEARCHMAP_HEADER
